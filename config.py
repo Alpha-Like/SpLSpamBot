1ae014a22fb44881d331b5e9d8bbefc4d8b37d2b
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-06 14:59:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class API:
+    API_ID = int(os.environ["API_ID"])
+    API_HASH = os.environ["API_HASH"]
+
+class TOKENS:
+    BOT_TOKEN = os.environ["BOT_TOKEN"]
+    BOT_TOKEN_2 = os.environ["BOT_TOKEN_2"]
+    BOT_TOKEN_3 = os.environ["BOT_TOKEN_3"]
+    BOT_TOKEN_4 = os.environ["BOT_TOKEN_4"]
+    BOT_TOKEN_5 = os.environ["BOT_TOKEN_5"]
+    BOT_TOKEN_6 = os.environ["BOT_TOKEN_6"]
+    BOT_TOKEN_7 = os.environ["BOT_TOKEN_7"]
+    BOT_TOKEN_8 = os.environ["BOT_TOKEN_8"]
+    BOT_TOKEN_9 = os.environ["BOT_TOKEN_9"]
+    BOT_TOKEN_10 = os.environ["BOT_TOKEN_10"]
+
+class OWNER:
+    OWNER_ID = os.environ["OWNER_ID"]
