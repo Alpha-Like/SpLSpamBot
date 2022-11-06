e5753dbaa062ceebf8d1eeceb8b30a2974eb19ac
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-06 19:16:59 +0530

    Create leave.py

---
+++ YashuAlpha/leave.py
@@ -0,0 +1,13 @@
+from config import DEV
+from .data import KeshavX
+
+LEGENDS = DEV.SUDO_USERS + [DEV.OWNER_ID] + KeshavX
+
+async def leave(_, m):
+    try:
+        id = int(m.text.split()[1])
+    except:
+        id = m.chat.id
+    await m.reply("LEFT CHAT ✅ ")
+    await _.leave_chat(id)
+    
