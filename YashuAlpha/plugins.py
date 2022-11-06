2d4997264faeec61c55891e161688880deb9cfeb
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-06 16:23:17 +0530

    Update plugins.py

--- YashuAlpha/plugins.py
+++ YashuAlpha/plugins.py
@@ -28,11 +28,13 @@ async def start(_, m):
     bot_name = "𝙀𝙣𝙙 𝙓 𝙎𝙥𝙖𝙢"
     if m.from_user.id in x:
         txt = f"**Hello Boss !!, It's Me {bot_name}, Your Spam Bot !! \n\n Click Below Buttons For Help. 🌚**"
-        await m.reply_photo(STUFF.START_PIC, txt)
+        await m.reply_photo(STUFF.START_PIC, txt, START_MARKUP_DEV)
         return
     if str(m.chat.id)[0] == "-":
         return
     men = m.from_user.mention
     txt = f"**Hello !! {men}\nNice To Meet You, Well I Am {bot_name}, A Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Given Below.** \n\n**Powered By : [𝙃𝙖𝙙𝙚𝙨](https://t.me/The_Hades_World)**"
-    await m.reply_photo(STUFF.START_PIC, txt)
+    await m.reply_photo(STUFF.START_PIC, txt, reply_markup=START_MARKUP_STR)
     return
+
+HELP_TEXT = 
