dae07b6ef6d5646a5314a4f00eb103b6c7a69ab9
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-06 17:12:28 +0530

    Update spam.py

--- YashuAlpha/spam.py
+++ YashuAlpha/spam.py
@@ -42,6 +42,13 @@ async def spam(_, m):
                 count = int(m.text.split()[1])
             except:
                 return await m.reply(f"{hl}spam [count]")
+        else:
+            x = None
+            try:
+                count = int(m.text.split()[1])
+                txt = m.text.split(None, 1)[2]
+            except:
+                return await m.reply(f"{hl}spam [count] [text]")
     else:
         try:
             count = int(m.text.split()[1])
@@ -49,8 +56,38 @@ async def spam(_, m):
         except:
             return await m.reply(f"{hl}spam [count] [text]")
 
+    for x in range(0, count):
+        if y.photo:
+            try:
+                await _.send_photo(m.chat.id, x)
+            except Exception as e:
+                print(e)
+                pass
+        elif y.video:
+            try:
+                await _.send_video(m.chat.id, x)
+            except Exception as e:
+                print(e)
+                pass
+        elif y.audio:
+            try:
+                await _.send_audio(m.chat.id, x)
+            except Exception as e:
+                print(e)
+                pass
+        elif y.document:
+            try:
+                await _.send_document(m.chat.id, x)
+            except Exception as e:
+                print(e)
+                pass
+        else:
+            try:
+                await _.send_message(m.chat.id, txt)
+            except Exception as e:
+                print(e)
+                pass
         
-            
         
         
         
