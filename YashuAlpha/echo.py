d3a0ed2abf5396d3ab2e66fc582e10b3f2b7ec0e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-06 18:56:33 +0530

    Update echo.py

--- YashuAlpha/echo.py
+++ YashuAlpha/echo.py
@@ -3,6 +3,17 @@ from config import DEV, STUFF
 hl = STUFF.COMMAND_HANDLER
 
 ECHO_USERS = []
-
 async def echo(_, m):
+    global RAID_IDS
+    try:
+        if m.reply_to_message:
+            id = m.reply_to_message.from_user.id
+        else:
+            x = m.text.split()[1]
+            if str(x)[0] == "@":
+                id = (await _.get_users(x)).id
+            else:
+                id = int(x)
+    except:
+        return await m.reply(f"{hl}addecho [username|id|reply]")
     
