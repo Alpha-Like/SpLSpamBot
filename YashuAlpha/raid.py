b14e343723c5f76283279203b8d7bbea38bee88d
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2023-01-16 10:50:33 +0530

    Update raid.py

--- YashuAlpha/raid.py
+++ YashuAlpha/raid.py
@@ -5,6 +5,7 @@ import asyncio
 from .data import KeshavX
 from YashuAlpha.Database.replyraid import *
 from YashuAlpha.Database.sudo import get_sudos
+from .verify import verify
 
 hl = STUFF.COMMAND_HANDLER
 
@@ -13,6 +14,8 @@ LEGENDS = DEV.SUDO_USERS + [DEV.OWNER_ID] + KeshavX
 SUDOS = DEV.SUDO_USERS
 
 async def raid(_, m):
+    if not await verify(m.from_user.id):
+        return
     try:
         count = int(m.text.split()[1])
     except:
@@ -20,14 +23,16 @@ async def raid(_, m):
     for c in range(0, count):
         raid = random.choice(RAID)
         if m.reply_to_message:
-            if m.reply_to_message.from_user.id in LEGENDS:
-                return await m.reply("CAN'T RAID THEM !")
+            if await verify (m.reply_to_message.from_user.id):
+                return await m.reply("`CAN'T RAID THEM !`")
             await m.reply_to_message.reply(raid)
         else:
             await _.send_message(m.chat.id, raid)
         await asyncio.sleep(0.02)
 
 async def replyraid(_, m):
+    if not await verify(m.from_user.id):
+        return
     try:
         if m.reply_to_message:
             id = m.reply_to_message.from_user.id
@@ -39,14 +44,16 @@ async def replyraid(_, m):
                 id = int(x)
     except:
         return await m.reply(f"`{hl}replyraid [id|username|reply]`")
-    if id in LEGENDS:
+    if await verify(id):
         return await m.reply("`CAN'T RAID THEM !`")
-    if await is rr(id):
+    if await is_rr(id):
         return await m.reply("`RAID IS ALREADY ACTIVATED TO THIS USER !`")
     await add_rr(id)
     return await m.reply(f"`RAID REPLY ACTIVATED TO USER `<code>{id}</code>")
     
 async def dreplyraid(_, m):
+    if not await verify(m.from_user.id):
+        return
     try:
         if m.reply_to_message:
             id = m.reply_to_message.from_user.id
@@ -58,16 +65,12 @@ async def dreplyraid(_, m):
                 id = int(x)
     except:
         return await m.reply(f"`{hl}dreplyraid [id|username|reply]`")
-    
     if not await is_rr(id):
         return await m.reply("`RAID IS NOT ACTIVATED TO THIS USER !`")
     await del_rr(id)
     return await m.reply(f"`RAID REPLY DEACTIVATED TO USER `<code>{id}</code>")
 
 async def raid_cwf(_, m):
-    global SUDOS
-    if not SUDOS:
-        SUDOS = await get_sudos()
     if m.from_user:
         if await is_rr(m.from_user.id):
             await m.reply(random.choice(REPLYRAID))
