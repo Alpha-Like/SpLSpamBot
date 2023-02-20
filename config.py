d028169ede0fde7da1100b3f3c38c3938547c1b0
Keshava Tripathi <124826956+ShutupKeshav@users.noreply.github.com>
2023-02-20 21:55:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,34 @@
 import os
 
 class API:
-    API_ID = int(os.environ["API_ID"])
-    API_HASH = os.environ["API_HASH"]
+    API_ID = int(os.getenv("API_ID", ""))
+    API_HASH = os.geten(["API_HASH", "")
 
 class TOKENS:
-    BOT_TOKEN = os.environ["BOT_TOKEN"]
-    BOT_TOKEN_2 = os.environ["BOT_TOKEN_2"]
-    BOT_TOKEN_3 = os.environ["BOT_TOKEN_3"]
-    BOT_TOKEN_4 = os.environ["BOT_TOKEN_4"]
-    BOT_TOKEN_5 = os.environ["BOT_TOKEN_5"]
-    BOT_TOKEN_6 = os.environ["BOT_TOKEN_6"]
-    BOT_TOKEN_7 = os.environ["BOT_TOKEN_7"]
-    BOT_TOKEN_8 = os.environ["BOT_TOKEN_8"]
-    BOT_TOKEN_9 = os.environ["BOT_TOKEN_9"]
-    BOT_TOKEN_10 = os.environ["BOT_TOKEN_10"]
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    BOT_TOKEN_2 = os.getenv("BOT_TOKEN_2", "")
+    BOT_TOKEN_3 = os.getenv("BOT_TOKEN_3", "")
+    BOT_TOKEN_4 = os.getenv("BOT_TOKEN_4", "")
+    BOT_TOKEN_5 = os.getenv("BOT_TOKEN_5", "")
+    BOT_TOKEN_6 = os.getenv("BOT_TOKEN_6", "")
+    BOT_TOKEN_7 = os.getenv("BOT_TOKEN_7", "")
+    BOT_TOKEN_8 = os.getenv("BOT_TOKEN_8", "")
+    BOT_TOKEN_9 = os.getenv("BOT_TOKEN_9", "")
+    BOT_TOKEN_10 = os.getenv("BOT_TOKEN_10", "")
 
 class DATABASE:
-    MONGO_DB_URL = os.environ["MONGO_DB_URL"]
+    MONGO_DB_URL = os.getenv("MONGO_DB_URL", "")
 
 class DEV:
-    OWNER_ID = int(os.environ["OWNER_ID"])
+    OWNER_ID = int(os.getenv("OWNER_ID", ""))
     
     # DONT EDIT THIS 
     SUDO_USERS = [5834211089] 
     # YOU CAN ADD SUDO USING /addsudo
 
 class STUFF:
-    ALIVE_PIC = os.environ["ALIVE_PIC"]
-    HELP_PIC = os.environ["HELP_PIC"]
-    START_PIC = os.environ["START_PIC"]
-    COMMAND_HANDLER = os.environ["COMMAND_HANDLER"]
+    ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+    HELP_PIC = os.getenv("HELP_PIC", "")
+    START_PIC = os.getenv("START_PIC", "")
+    COMMAND_HANDLER = os.getenv("COMMAND_HANDLER", "")
+    ALLOW_PORN = os.getenv("ALLOW_PORN", True) # REPLACE 'True' BY 'False' IF U WANT TO DISABLE PORN
