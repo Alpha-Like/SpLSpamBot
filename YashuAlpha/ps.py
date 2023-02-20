d9484c36fbb1c3ca3d8e2ba846e0fb701e875503
Keshava Tripathi <124826956+ShutupKeshav@users.noreply.github.com>
2023-02-20 21:50:16 +0530

    Create ps.py

---
+++ YashuAlpha/ps.py
@@ -0,0 +1,18 @@
+from .verify import verify
+from .data import PORMS
+import random
+from config import STUFF
+
+hl = STUFF.COMMAND_HANDLER
+
+async def ps(_, m):
+  if not await verify(m.from_user.id):
+    return
+  if not STUFF.ALLOW_PORN:
+    return await m.reply("Porn spam is disabled ! For enabling, set ALLOW_PORN to True in config !")
+  try:
+    count = int(m.text.split()[1])
+  except:
+    return await m.reply(f"{hl}pornspam | {hl}ps [count]")
+  for i in range(0, count):
+    await _.send_animation(m.chat.id, random.choice(PORMS))
